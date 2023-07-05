737c383c34dedf3e430df9761453b8b7c377557e
vinicius <vinicius.henrique.92@hotmail.com>
2023-07-04 21:21:54 -0300

    fix: adjusting the source of images

--- produtos/blusas-e-casacos/image-creator.js
+++ produtos/blusas-e-casacos/image-creator.js
@@ -2,6 +2,6 @@ import { createPhoto } from "../../scripts/create-photo.js"
 
 const imageContainer = document.getElementById('image-container')
 const maxImages = 36
-const caminho = '/imagens/Blusas-e-casacos/Blusas-e-casacos-Amiri/amiri-'
+const caminho = '../../imagens/Blusas-e-casacos/Blusas-e-casacos-Amiri/amiri-'
 
 createPhoto(maxImages, imageContainer, caminho)
