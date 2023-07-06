138d5bf91011e0e8de9a083f0887c95b3cf3c8a4
vinicius <vinicius.henrique.92@hotmail.com>
2023-07-06 17:01:49 -0300

    feat: calcas e shorts amiri - page done

---
+++ produtos/calcas-e-shorts-amiri/amiri/image-creator.js
@@ -0,0 +1,7 @@
+import { createPhoto } from "../../../scripts/create-photo.js"
+
+const imageContainer = document.getElementById('image-container')
+const maxImages = 71
+const caminho = '../../../imagens/Calcas-e-shorts-Amiri/amiri-'
+
+createPhoto(maxImages, imageContainer, caminho)
