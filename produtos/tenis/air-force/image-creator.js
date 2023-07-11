d609cb1d72efb85afd604f3818d67ece96a94859
vinicius <vinicius.henrique.92@hotmail.com>
2023-07-11 19:28:33 -0300

    fix: adjusted the tenis page and reduced the size of some images

--- produtos/tenis/air-force/image-creator.js
+++ produtos/tenis/air-force/image-creator.js
@@ -1,7 +1,7 @@
 import { createPhoto } from "../../../scripts/create-photo.js"
 
 const imageContainer = document.getElementById('image-container')
-const maxImages = 19
+const maxImages = 16
 const caminho = '../../../imagens/Tenis/Tenis-Air-Force/air-force-'
 
 createPhoto(maxImages, imageContainer, caminho)
