12699e7d3d91d203302d9caec50cd9b30f8f86f7
vinicius <vinicius.henrique.92@hotmail.com>
2023-07-12 08:58:35 -0300

    feat: added more palm angels shirts

--- produtos/tshirts-camisetas/palm-angels/image-creator.js
+++ produtos/tshirts-camisetas/palm-angels/image-creator.js
@@ -1,7 +1,7 @@
 import { createPhoto } from "../../../scripts/create-photo.js"
 
 const imageContainer = document.getElementById('image-container')
-const maxImages = 32
+const maxImages = 44
 const caminho = '../../../imagens/Camisas/Camisas-Palm-Angels/palm-angels-'
 
 createPhoto(maxImages, imageContainer, caminho)
